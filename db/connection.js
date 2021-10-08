185581ffc45eeecc9e6328dd601d88d4cc642801
jondagamkd <johntaylorwoodland@gmail.com>
2021-10-07 18:19:10 -0700

    Error update

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection(
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
-      password: 'necrolite2',
+      password: '67576567567',
       database: 'election'
     },
     console.log('Connected to the election database.')
